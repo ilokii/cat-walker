75a28e6db5c5ca95616747c07824ca123b2c59fa
wudongloki <wudongloki@gmail.com>
2025-02-06 23:24:09 +0800

    fix 每日任务配置更新

--- miniprogram/data/dailyTasks.js
+++ miniprogram/data/dailyTasks.js
@@ -1,26 +1,31 @@
 module.exports = [
   {
     id: 1,
-    steps: 2000,
+    steps: 0,
     reward: 1
   },
   {
     id: 2,
+    steps: 2000,
+    reward: 1
+  },
+  {
+    id: 3,
     steps: 4000,
     reward: 2
   },
   {
-    id: 3,
+    id: 4,
     steps: 6000,
     reward: 3
   },
   {
-    id: 4,
+    id: 5,
     steps: 8000,
     reward: 4
   },
   {
-    id: 5,
+    id: 6,
     steps: 10000,
     reward: 5
   }
